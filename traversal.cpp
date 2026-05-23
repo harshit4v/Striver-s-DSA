c990db9ec6912087cd30eb40ded4e2522123f5a5
harshit4v <harshitverma1773@gmail.com>
2026-05-24 01:55:55 +0530

    all traversals

--- traversal.cpp
+++ traversal.cpp
@@ -17,6 +17,22 @@ void inorder(Node *root,vector<int>&arr){
     arr.push_back(root->data);
     inorder(root->right,arr);
 }
+void preOrder(Node *root,vector<int>&arr2){
+    if(root==nullptr){
+        return;
+    }
+    arr2.push_back(root->data);
+    preOrder(root->left,arr2);
+    preOrder(root->right,arr2);
+}
+void postOrder(Node *root,vector<int>&arr3){
+    if(root==nullptr){
+        return ;
+    }
+    postOrder(root->left,arr3);
+    postOrder(root->right,arr3);
+    arr3.push_back(root->data);
+}
 int main(){
     Node *root=new Node(1);
     root->left=new Node(2);
@@ -30,8 +46,24 @@ int main(){
     root->right->right->right=new Node(10);
     vector<int>arr;
     inorder(root,arr);
+    cout<<"INORDER TRAVERSAL";
     for(int i=0;i<arr.size();i++){
         cout<<arr[i]<<" ";
     }
+    cout<<endl;
+    cout<<"PREORDER TRAVERSAL";
+    vector<int>arr2;
+    preOrder(root,arr2);
+    for(int i=0;i<arr2.size();i++){
+        cout<<arr2[i]<<" ";
+    }
+    cout<<endl;
+    cout<<"POSTORDER TRAVERSAL";
+    vector<int>arr3;
+    postOrder(root,arr3);
+    for(int i=0;i<arr3.size();i++){
+        cout<<arr3[i]<<" ";
+    }
+    cout<<endl;
     return 0;
 }
